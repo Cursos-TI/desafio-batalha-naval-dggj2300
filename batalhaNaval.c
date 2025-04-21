316332153e2fc45e0a2f55dee81a4e4b6f98b176
dggj2300 <166646489+dggj2300@users.noreply.github.com>
2025-04-21 17:33:22 +0000

    NIVEL NOVATO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,88 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    
+    //INICIALIZANDO O TABULEIRO
+    int i, j;
+    int tabuleiro[10][10];
+
+    //PREENCHENDO O TABULEIRO COM 0
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }    
+
+    //DETERMINANDO O TAMANHO E POSIÇÃO DO NAVIO 1
+    int linha1 = 1;
+    int coluna1 = 3;
+    int tamanho1 = 3;
+
+    //VERIFICANDO SE O NAVIO NÃO ULTRAPASSA O TAMANHO DO TABULEIRO
+    if (coluna1 + tamanho1 <= 10) {
+        int sobreposicao = 0;
+        //VERIFICANDO SE O NAVIO NÃO ESTÁ SE SOBREPONDO SOBRE OUTRO
+        for (j = coluna1; j < coluna1 + tamanho1; j++) {
+            if (tabuleiro[linha1][j] != 0) {
+                sobreposicao = 1;
+                break;
+            }
+        }
+        //POSICIONANDO O NAVIO PREENCHENDO COM NUMERO 3
+        if (sobreposicao == 0) {
+            for (j = coluna1; j < coluna1 + tamanho1; j++) {
+                tabuleiro[linha1][j] = 3;
+            }
+        } else {
+            printf("Erro: sobreposição detectada no navio 1. \n");
+        }
+    } else {
+        printf("Erro: navio 1 ultrapassa os limites do tabuleiro. \n");
+    }
+
+    //DETERMINANDO O TAMANHO E POSIÇÃO DO NAVIO 2
+    int linha2 = 4;
+    int coluna2 = 0;
+    int tamanho2 = 3;
+
+    //VERIFICANDO SE O NAVIO NÃO ULTRAPASSA O TAMANHO DO TABULEIRO
+    if (linha2 + tamanho2 <= 10) {
+        int sobreposicao = 0;
+        //VERIFICANDO SE O NAVIO NÃO ESTÁ SE SOBREPONDO SOBRE OUTRO
+        for (i = linha2; i < linha2 + tamanho2; i++) {
+            if(tabuleiro[i][coluna2] != 0) {
+                sobreposicao = 1;
+                break;
+            }
+        }
+        //POSICIONANDO O NAVIO PREENCHENDO COM NUMERO 3
+        if (sobreposicao == 0) {
+            for (i = linha2; i < linha2 + tamanho2; i++) {
+                tabuleiro[i][coluna2] = 3;
+            }
+        } else {
+            printf("Erro: sobreposição detectada no navio 2.\n");
+        }
+    } else {
+            printf("Erro: navio 2 ultrapassa os limites do tabuleiro.\n");
+    }
+
+    //COLOCANDO AS LETRAS NA PARTE DE CIMA DO TABULEIRO
+    printf("   ");
+    for (j = 0; j < 10; j++) {
+        printf("%c", 'A' + j);
+    }
+    printf("\n");
+
+    //PRINTANDO O TABULEIRO COM A NUMERAÇÃO NA LATERAL
+    for (i = 0; i < 10; i++) {
+        printf("%2d ", i + 1);
+        for (j = 0; j < 10; j++) {
+            printf("%d", tabuleiro[i][j]);
+        }        
+        printf("\n");
+    }
+
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
